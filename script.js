f83bf2f5b608bb79d7909316343c659af083d5a1
Glitch (glitch-hello-website) <none>
2021-06-12 21:30:58 -0500

    save button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let countEl = document.getElementById('count-el');
 let count = 0;
+let saved = 0;
 
 function increment(){
   count += 1
@@ -7,3 +8,10 @@ function increment(){
   console.log('Clicked');
 }
 
+function save(){
+  saved = count;
+  if(count==1){
+    document.getElementById('saved-text').innerText = "Work Chekpoint :  " + saved
+  }else{document.getElementById('saved-text').append(" - " + saved)}
+
+} 
